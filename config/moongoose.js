35142736f6f234a19ec2fc7f7ff1d5d151bb747c
Aman Shinde <1703amanshinde98@gmail.com>
2023-07-31 23:50:27 +0530

    mongo setup

--- config/moongoose.js
+++ config/moongoose.js
@@ -1,16 +1,28 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const main = async () => {
-  await mongoose.connect(process.env.MONGO_CONNECTION_URL);
-};
+// const main = async () => {
+//   await mongoose.connect(process.env.MONGO_CONNECTION_URL);
+// };
 
-main().catch((err) => console.log(err));
+// main().catch((err) => console.log(err));
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "error in connecting to DB"));
+// db.once("open", () => {
+//   console.log(process.env.MONGO_CONNECTION_URL);
+//   console.log("connected to DB successfully");
+// });
+
+mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "error in connecting to DB"));
-db.once("open", () => {
-  console.log("connected to DB successfully");
+db.on("error", console.error.bind(console, "Error in connecting to MongoDB"));
+db.once("open", function () {
+  console.log("Connected to Database :: Mongodb");
 });
 
 module.exports = mongoose;
