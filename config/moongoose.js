67a8eb9300bb8698c38dd135430c806c0ae2a326
Aman Shinde <1703amanshinde98@gmail.com>
2023-07-31 22:16:43 +0530

    Add remote mongodb

--- config/moongoose.js
+++ config/moongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const main = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/placement_cell");
+  await mongoose.connect(
+    "mongodb+srv://1703amanshinde98:eteTKBvDSdMdVcbD@aman.evnkzhp.mongodb.net/"
+  );
 };
 
 main().catch((err) => console.log(err));
